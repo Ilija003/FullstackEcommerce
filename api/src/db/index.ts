51e94ea3ca41efb2f6e0395ffd0b2affba9f001b
ilija003 <ilijapejic.pejica@gmail.com>
2025-02-14 10:39:46 +0100

    Product CRUD

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,8 +1,7 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-
-const pool = new Pool({
+const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL!,
 });
 
