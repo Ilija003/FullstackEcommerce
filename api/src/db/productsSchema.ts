8edec25e301b863df0c1efd47870a7637ed1d6a2
ilija003 <ilijapejic.pejica@gmail.com>
2025-02-15 19:09:01 +0100

    Validation with zod, create product and update product

--- api/src/db/productsSchema.ts
+++ api/src/db/productsSchema.ts
@@ -1,9 +1,20 @@
-import { integer, pgTable, varchar, text, doublePrecision } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  text,
+  doublePrecision,
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema } from 'drizzle-zod';
 
-export const productsTable = pgTable("products", {
+export const productsTable = pgTable('products', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   description: text(),
   image: varchar({ length: 255 }),
   price: doublePrecision().notNull(),
 });
+
+export const createProductSchema = createInsertSchema(productsTable);
+
+export const updateProductSchema = createInsertSchema(productsTable).partial();
