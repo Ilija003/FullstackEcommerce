301499b78a76e0e30f4f4e62bdaa6ddc77a91b10
ilija003 <ilijapejic.pejica@gmail.com>
2025-03-01 16:22:43 +0100

    Shoping cart

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,20 +1,37 @@
-import "@/global.css";
-import { Stack} from "expo-router";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import '@/global.css';
+import { Link, Stack } from 'expo-router';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
+import { ShoppingCart } from 'lucide-react-native';
+import { Pressable } from 'react-native';
+import { useCart } from '@/store/cartStore';
 
 const queryClient = new QueryClient();
-export default function RootLayout (){
-    return( 
-    <QueryClientProvider client={queryClient}>
-    <GluestackUIProvider>
-              <Stack>
-        <Stack.Screen name="index" options={{ title: 'Shop' }} />
-        <Stack.Screen name="product/[id]" options={{ title: 'Product' }} />
-      </Stack>
-    </GluestackUIProvider>
-    </QueryClientProvider>
-    
+export default function RootLayout() {
 
-    )
+    const cartItemsNum = useCart((state) => state.items.length);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GluestackUIProvider>
+        <Stack
+          screenOptions={{
+            headerRight: () => 
+                cartItemsNum > 0 && (
+              <Link href={'/cart'} asChild>
+                <Pressable className="flex-row gap-2">
+                <Icon as={ShoppingCart} />
+                <Text>{cartItemsNum}</Text>
+                </Pressable>
+              </Link>
+            ),
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: 'Shop' }} />
+          <Stack.Screen name="product/[id]" options={{ title: 'Product' }} />
+        </Stack>
+      </GluestackUIProvider>
+    </QueryClientProvider>
+  );
 }
--- ecommerce-mobile/app/product/[id].tsx
+++ ecommerce-mobile/app/product/[id].tsx
@@ -9,14 +9,21 @@ import { Button, ButtonText } from '@/components/ui/button';
 import { useQuery } from '@tanstack/react-query';
 import { fetchProductById } from '@/api/products';
 import { ActivityIndicator } from 'react-native';
+import { useCart } from '@/store/cartStore';
 
 export default function ProductDetailsScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
+  const addProduct = useCart((state) => state.addProduct);
+  
   const { data: product, isLoading, error } = useQuery({
     queryKey: ['products', id],
     queryFn: () => fetchProductById(Number(id)),
   });
+
+  const addToCart = () => {
+    addProduct(product);
+  }
   if(isLoading){
     return <ActivityIndicator />
   }
@@ -45,7 +52,7 @@ export default function ProductDetailsScreen() {
           <Text size="sm">{product.description}</Text>
         </VStack>
         <Box className="flex-col sm:flex-row">
-          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+          <Button onPress={addToCart} className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
             <ButtonText size="sm">Add to cart</ButtonText>
           </Button>
           <Button
