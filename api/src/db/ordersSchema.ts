898c4d6a69d9cc330b98225fcb6014c06b5f50f1
ilija003 <ilijapejic.pejica@gmail.com>
2025-03-26 18:56:49 +0100

    genezio deploy

--- api/src/db/ordersSchema.ts
+++ api/src/db/ordersSchema.ts
@@ -1,40 +1,52 @@
-import { doublePrecision, integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
-import { usersTable } from "./usersSchema";
-import { productsTable } from "./productsSchema";
-import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod";
+import {
+  doublePrecision,
+  integer,
+  pgTable,
+  timestamp,
+  varchar,
+} from 'drizzle-orm/pg-core';
+import { usersTable } from './usersSchema.js';
+import { productsTable } from './productsSchema.js';
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from 'zod';
 
 export const ordersTable = pgTable('orders', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    createdAt: timestamp().notNull().defaultNow(),
-    status: varchar({length: 50}).notNull().default('New'),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  createdAt: timestamp().notNull().defaultNow(),
+  status: varchar({ length: 50 }).notNull().default('New'),
 
-    userId: integer().references(()=>usersTable.id).notNull(),
+  userId: integer()
+    .references(() => usersTable.id)
+    .notNull(),
 });
 
 export const orderItemsTable = pgTable('order_items', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    orderId: integer().references(() => ordersTable.id).notNull(),
-    productId: integer().references(() => productsTable.id).notNull(),
-    quantity: integer().notNull(),
-    price: doublePrecision().notNull(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  orderId: integer()
+    .references(() => ordersTable.id)
+    .notNull(),
+  productId: integer()
+    .references(() => productsTable.id)
+    .notNull(),
+  quantity: integer().notNull(),
+  price: doublePrecision().notNull(),
 });
 
 export const insertOrderSchema = createInsertSchema(ordersTable).omit({
-    userId: true,
-    status: true,
-    createdAt: true,
+  userId: true,
+  status: true,
+  createdAt: true,
 });
 
 export const insertOrderItemSchema = createInsertSchema(orderItemsTable).omit({
-    orderId: true
+  orderId: true,
 });
 
 export const insertOrderWithItemsSchema = z.object({
-    order: insertOrderSchema,
-    items: z.array(insertOrderItemSchema),
-  });
+  order: insertOrderSchema,
+  items: z.array(insertOrderItemSchema),
+});
 
 export const updateOrderScehma = createInsertSchema(ordersTable).pick({
-    status: true,
+  status: true,
 });
